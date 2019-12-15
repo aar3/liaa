a7b748203f6562746400045d870e1470dcac5940
Rashad <Alston@Rashads-MacBook-Air.local>
2019-12-15 12:38:03 -0800

    update unittests

--- tests/test_node.py
+++ tests/test_node.py
@@ -7,49 +7,59 @@ from kademlia.utils import hex_to_base_int
 
 
 class TestNode:
-    def test_long_id(self):
-        rid = hashlib.sha1(str(random.getrandbits(255)).encode()).digest()
-        node = Node(rid)
-        assert node.long_id == hex_to_base_int(rid.hex())
+	def test_long_id(self):  # pylint: disable=no-self-use
+		rid = hashlib.sha1(str(random.getrandbits(255)).encode()).digest()
+		node = Node(rid)
+		assert node.long_id == hex_to_base_int(rid.hex())
 
-    def test_distance_calculation(self):
-        ridone = hashlib.sha1(str(random.getrandbits(255)).encode())
-        ridtwo = hashlib.sha1(str(random.getrandbits(255)).encode())
+	def test_distance_calculation(self):  # pylint: disable=no-self-use
+		ridone = hashlib.sha1(str(random.getrandbits(255)).encode())
+		ridtwo = hashlib.sha1(str(random.getrandbits(255)).encode())
 
-        shouldbe = hex_to_base_int(ridone.digest().hex()) ^ hex_to_base_int(ridtwo.digest().hex())
-        none = Node(ridone.digest())
-        ntwo = Node(ridtwo.digest())
-        assert none.distance_to(ntwo) == shouldbe
+		shouldbe = hex_to_base_int(ridone.digest().hex()) ^ hex_to_base_int(ridtwo.digest().hex())
+		none = Node(ridone.digest())
+		ntwo = Node(ridtwo.digest())
+		assert none.distance_to(ntwo) == shouldbe
 
 
 class TestNodeHeap:
-    def test_max_size(self, mknode):
-        node = NodeHeap(mknode(intid=0), 3)
-        assert not node
+	"""
+	For test_max_size and test_iteration:
 
-        for digit in range(10):
-            node.push(mknode(intid=digit))
+	NodeHeap will always have len <= NodeHeap.max_size, and
+	NodeHeap.__iter__ will only return `n` nodes that have asmallest
+	distance from NodeHeap.node, where `n` <= NodeHeap.max_size
+	"""
 
-        assert len(node) == 3
-        assert len(list(node)) == 3
+	def test_max_size(self, mknode):  # pylint: disable=no-self-use
+		node = NodeHeap(mknode(intid=0), 3)
+		assert not node
 
-    def test_iteration(self, mknode):
-        heap = NodeHeap(mknode(intid=0), 5)
-        nodes = [mknode(intid=x) for x in range(10)]
-        for index, node in enumerate(nodes):
-            heap.push(node)
-        for index, node in enumerate(heap):
-            assert index == node.long_id
-            assert index < 5
+		for digit in range(10):
+			node.push(mknode(intid=digit))
 
-    def test_remove(self, mknode):
-        heap = NodeHeap(mknode(intid=0), 5)
-        nodes = [mknode(intid=x) for x in range(10)]
-        for node in nodes:
-            heap.push(node)
+		assert len(node) == 3
+		assert len(list(node)) == 3
 
-        heap.remove([nodes[0].id, nodes[1].id])
-        assert len(list(heap)) == 5
-        for index, node in enumerate(heap):
-            assert index + 2 == node.long_id
-            assert index < 5
+	def test_iteration(self, mknode):  # pylint: disable=no-self-use
+		heap = NodeHeap(mknode(intid=0), 5)
+		nodes = [mknode(intid=x) for x in range(10)]
+		for index, node in enumerate(nodes):
+			heap.push(node)
+
+		for index, node in enumerate(heap):
+			assert index == node.long_id
+			assert index < 5
+
+	def test_remove(self, mknode):  # pylint: disable=no-self-use
+		heap = NodeHeap(mknode(intid=0), 5)
+		nodes = [mknode(intid=x) for x in range(10)]
+		for node in nodes:
+			heap.push(node)
+
+		heap.remove([nodes[0].id, nodes[1].id])
+		assert len(list(heap)) == 5
+		for index, node in enumerate(heap):
+			# we removed to elements so offset index to account for it
+			assert index + 2 == node.long_id
+			assert index < 5
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,30 +1,30 @@
 import hashlib
 
-from kademlia.utils import *
+from kademlia.utils import (digest, shared_prefix, hex_to_base_int)
 
 
 class TestUtils:
-    def test_digest(self): 
-        dig = hashlib.sha1(b'1').digest()
-        assert dig == digest(1)
+	def test_digest(self):  # pylint: disable=no-self-use
+		dig = hashlib.sha1(b'1').digest()
+		assert dig == digest(1)
 
-        dig = hashlib.sha1(b'another').digest()
-        assert dig == digest('another')
+		dig = hashlib.sha1(b'another').digest()
+		assert dig == digest('another')
 
-    def test_shared_prefix(self):  
-        args = ['prefix', 'prefixasdf', 'prefix', 'prefixxxx']
-        assert shared_prefix(args) == 'prefix'
+	def test_shared_prefix(self):  # pylint: disable=no-self-use
+		args = ['prefix', 'prefixasdf', 'prefix', 'prefixxxx']
+		assert shared_prefix(args) == 'prefix'
 
-        args = ['p', 'prefixasdf', 'prefix', 'prefixxxx']
-        assert shared_prefix(args) == 'p'
+		args = ['p', 'prefixasdf', 'prefix', 'prefixxxx']
+		assert shared_prefix(args) == 'p'
 
-        args = ['one', 'two']
-        assert shared_prefix(args) == ''
+		args = ['one', 'two']
+		assert shared_prefix(args) == ''
 
-        args = ['hi']
-        assert shared_prefix(args) == 'hi'
+		args = ['hi']
+		assert shared_prefix(args) == 'hi'
 
-    def test_to_base16_int(self):
-        n = 5
-        n_as_bytes = bytes([n])
-        assert hex_to_base_int(n_as_bytes.hex()) == n
+	def test_to_base16_int(self):  # pylint: disable=no-self-use
+		num = 5
+		num_as_bytes = bytes([num])
+		assert hex_to_base_int(num_as_bytes.hex()) == num
